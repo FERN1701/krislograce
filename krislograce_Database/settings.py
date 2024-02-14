8a9f468623c178ff5bfc4efa613d83bfb5d74b56
Fern Aragon <95953827+FERN1701@users.noreply.github.com>
2024-02-15 00:13:47 +0800

    latest

--- krislograce_Database/settings.py
+++ krislograce_Database/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qrf#y0srh4dbl9xm8te3^@69a^a)l$qli#lz5fzt*6llw@7&2_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
